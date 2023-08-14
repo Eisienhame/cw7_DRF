f2b07d1ffa0e7f61d82e06d24f2df5701652d603
Eisiehame <knurov24@gmail.com>
2023-08-14 21:32:25 +0200

    =-

--- main/tasks.py
+++ main/tasks.py
@@ -1,13 +1,16 @@
-from django.conf import settings
-from telebot import TeleBot
+from config import settings
 from config.celery import app
 from main.models import Habit
+
+import telebot
+
 
 
 @app.task
 def send_telegram_message(habit_id):
     """Отправка сообщения через бот TG"""
     habit = Habit.objects.get(id=habit_id)
-    bot = TeleBot(settings.TG_BOT_TOKEN)
-    message = f"Напоминание о выполнении привычки {habit.action} в {habit.time} в {habit.place}"
+    bot = telebot.TeleBot(settings.TG_BOT_TOKEN)
+    message = f"Напоминание о выполнении привычки {habit.name}"
     bot.send_message(habit.owner.chat_id, message)
+
